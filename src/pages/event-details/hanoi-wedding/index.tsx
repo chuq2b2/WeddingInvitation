01dc2b04feb496c4db7c2bcfb77bb2cf43d9b2c5
Quynh Chu <98863661+chuq2b2@users.noreply.github.com>
2024-12-19 12:54:38 +0700

    Add Invite component

--- src/pages/event-details/hanoi-wedding/index.tsx
+++ src/pages/event-details/hanoi-wedding/index.tsx
@@ -1,6 +1,15 @@
+import EventInvite from "@/components/Invite";
+import { eventDetails } from "@/components/EventDetails";
+
 export default function HanoiWedding() {
+  const hanoi = eventDetails.hanoi;
   return (
     <>
+      <EventInvite
+        location={hanoi.location}
+        date={hanoi.date}
+        hotel={hanoi.hotel}
+      />
       <h1>Hanoi</h1>
     </>
   );
