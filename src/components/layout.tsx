9459253a650f22025043d4f40507b11ed5d66590
Quynh Chu <98863661+chuq2b2@users.noreply.github.com>
2025-02-11 20:50:27 -0500

    add analytics

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,16 @@
 import { ReactNode } from "react";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
+import { Analytics } from '@vercel/analytics/next';
+
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
       <NavBar />
-      <main>{children}</main>
+      <main>{children}
+      <Analytics />
+      </main>
       <Footer />
     </div>
   );
